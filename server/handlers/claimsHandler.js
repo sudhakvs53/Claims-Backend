9ad4b1c1964b248bfe2770296783dfccab566db4
JaitraUser <Abinash.Patra@jaitra.com>
2017-06-14 18:13:29 +0530

    minor changes

--- server/handlers/claimsHandler.js
+++ server/handlers/claimsHandler.js
@@ -1,39 +1,36 @@
 import claimModel from './../models/claimsModel';
 
 export default {
-    create_claim: create_claim,
-    search_by_projectId: search_by_projectId,
-    save_claim: save_claim
+    get_claim_defaults: get_claim_defaults
 };
 
-const create_claim = (newClaim) => {
-    newClaim.save((err) => {
-        if (err) {
-            console.log('err while creating claim ' + err);
-        } else {
-            console.log('claim created successfully');
-        }
-    });
-};
+// const get_claim_defaults = () => {
+//     claimModel.find({}, (err, data) => {
+//         if (err) {
+//             console.log('err while retrieving defaults ' + err);
+//         }
+//         else {
 
-const search_by_projectId = (projID, callback) => {
-    claimModel.find({ 'proj_id': projID }, (err, data) => {
-        if (err) {
-            console.log('err while searching by project id ' + err);
-        } else {
-            callback(data);
-        }
-    });
-};
+//         }
+//     })
+// }
 
-const save_claim = (newClaim, callback) => {
-    newClaim.save((err) => {
-        if (err) {
-            console.log('err while saving claim ' + err);
-            callback(false);
-        } else {
-            console.log('claim saved successfully');
-            callback(true);
-        }
-    });
-};
+// const create_claim = (newClaim) => {
+//     newClaim.save((err) => {
+//         if (err) {
+//             console.log('err while creating claim ' + err);
+//         } else {
+//             console.log('claim created successfully');
+//         }
+//     });
+// };
+
+// const search_by_projectId = (projID, callback) => {
+//     claimModel.find({ 'proj_id': projID }, (err, data) => {
+//         if (err) {
+//             console.log('err while searching by project id ' + err);
+//         } else {
+//             callback(data);
+//         }
+//     });
+// };
--- server/router.js
+++ server/router.js
@@ -4,20 +4,24 @@ import claimsHandler from './handlers/claimsHandler';
 
 const router = express.Router();
 
-router.get('/fetch/:proj_id', (req, res) => {
-    // res.sendFile(path.resolve());
-    claimsHandler.search_by_projectId(req.params.proj_id, (result) => {
-        res.json(result);
-    });
-});
-
-router.get('/create_claim', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, './../src/index.html'));
-    claimsHandler.create_claim();
-});
-
-router.get('/save_claim', () => {
+router.get('/get_claims_defaults', () => {
 
 });
+
+// router.get('/fetch/:proj_id', (req, res) => {
+//     // res.sendFile(path.resolve());
+//     claimsHandler.search_by_projectId(req.params.proj_id, (result) => {
+//         res.json(result);
+//     });
+// });
+
+// router.get('/create_claim', (req, res) => {
+//     // res.sendFile(path.resolve(__dirname, './../src/index.html'));
+//     claimsHandler.create_claim();
+// });
+
+// router.get('/save_claim', () => {
+
+// });
 
 export default router;
