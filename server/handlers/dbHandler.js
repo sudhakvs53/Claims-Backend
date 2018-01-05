d15e66e2720328ed0dcd54c2ce80370da606af1f
abinash <luffy.abinash@gmail.com>
2018-01-05 16:29:07 +0530

    moving to DiyaLabs git

--- server/handlers/dbHandler.js
+++ server/handlers/dbHandler.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
-import { dev } from './../config';
-const dburl = dev.dbUrl.mongoAtlasURL; //localDBUrl for local, mLabDBurl for cloud connection, mongoAtlasURL for latest cloud
+import config from './../config.json';
+const dburl = config.dev.dbUrl.mongoAtlasUrl; //localDBUrl for local, mLabDBurl for cloud connection, mongoAtlasURL for latest cloud
 
 const db = mongoose.connection;
 mongoose.Promise = global.Promise;
